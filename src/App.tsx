f8421f39dbac7dbf0b4d18c71e4b4840b78d2da0
Willians Faria <dev.willians.faria@gmail.com>
2023-04-03 00:43:05 -0300

    adding noto sans font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
+import {
+  useFonts,
+  NotoSans_400Regular,
+  NotoSans_700Bold,
+  NotoSans_900Black,
+} from '@expo-google-fonts/noto-sans';
 import { NavigationContainer } from '@react-navigation/native';
 import { registerRootComponent } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 import { darkTheme } from 'themes/darkTheme';
@@ -9,14 +17,36 @@ import { lightTheme } from 'themes/lightTheme';
 
 import { AuthRouter } from 'routers/AuthRouter';
 
+SplashScreen.preventAutoHideAsync();
+
 function App() {
   const [currentTheme] = useState('light');
+  const [fontsLoaded] = useFonts({
+    NotoSans_400Regular,
+    NotoSans_700Bold,
+    NotoSans_900Black,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <ThemeProvider theme={currentTheme === 'light' ? lightTheme : darkTheme}>
-        <AuthRouter />
-      </ThemeProvider>
-    </NavigationContainer>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <ThemeProvider
+          theme={currentTheme === 'light' ? lightTheme : darkTheme}
+        >
+          <AuthRouter />
+        </ThemeProvider>
+      </NavigationContainer>
+    </View>
   );
 }
 
